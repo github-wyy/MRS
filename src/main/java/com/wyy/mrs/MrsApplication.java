4a02c3f64bd43b8efea1956fcc410f9e70e6636f
wyy <202041566@qq.com>
2023-03-16 17:13:44 +0800

    fix:run

--- src/main/java/com/wyy/mrs/MrsApplication.java
+++ src/main/java/com/wyy/mrs/MrsApplication.java
@@ -1,8 +1,5 @@
 package com.wyy.mrs;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,9 +14,6 @@ public class MrsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MrsApplication.class, args);
-        LocalDateTime creationTime = LocalDateTime.now();
-        String date= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(creationTime);
-        System.out.println(date);
     }
 
 }
